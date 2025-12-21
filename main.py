e3cbe05cb8fc2f865e8eed960bb7031f6888958b
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2025-12-21 11:24:56 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+pages = {
+    "Builder": [
+        st.Page("character.py", title="Character Prompt Builder", icon="🧙‍♂️"),
+    ]
+}
+
+def main():
+    pg = st.navigation(pages)
+    pg.run()
+
+if __name__ == "__main__":
+    main()
